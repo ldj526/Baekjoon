255c665ccf237db4cab0c6bffe471df03366811c
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-01-05 23:39:07 +0900

    Create 15655.cpp

---
+++ BackTracking/15655.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int arr[10], arr2[10];
+bool isused[10];
+
+void func(int a, int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = a; i <= n; i++)
+	{
+		if (!isused[i]) {
+			arr[k] = arr2[i - 1];
+			isused[i] = true;
+			func(i, k + 1);
+			isused[i] = false;
+		}
+	}
+
+}
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr2[i];
+	sort(arr2, arr2 + n);
+	func(1, 0);
+}
