ffaf36139f16b145952a5cc61da9c5be7e4ee4ee
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-01-10 00:35:20 +0900

    Create 15650_2.cpp

---
+++ BackTracking/15650_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int arr[10];
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = m; i < n; i++)
+		arr[i] = 1;
+	do {
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == 0)
+				cout << i+1 << ' ';
+		}
+		cout << '\n';
+	} while (next_permutation(arr, arr + n));
+}
