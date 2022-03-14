00dc40d0180c8fc6d1030945814bcb4a2f8f6eaa
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-03-14 23:26:15 +0900

    Update 10814.cpp

--- Sort/10814.cpp
+++ Sort/10814.cpp
@@ -19,3 +19,27 @@ int main(void) {
 	for (int i = 0; i < n; i++)
 		cout << vec[i].first << ' ' << vec[i].second << '\n';
 }
+
+ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+using namespace std;
+int n, age;
+string name;
+vector<tuple<int, int, string>> v;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> age >> name;
+		v.push_back({age, i, name });
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++)
+		cout << get<0> (v[i]) << ' ' << get<2> (v[i]) << '\n';
+}
