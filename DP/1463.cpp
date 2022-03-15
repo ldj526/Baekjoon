13c606e9ec69f34961851edeaa2e84302d489867
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-03-15 20:32:17 +0900

    Create 1463.cpp

---
+++ DP/1463.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int num;
+int dp[1000005];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> num;
+	dp[0] = 1;
+	for (int i = 2; i <= num; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+		if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+	}
+	cout << dp[num];
+}
