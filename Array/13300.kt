3e26c18345a4e14dc45a915b8f1a8b1110f7f627
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-06-18 23:38:52 +0900

    Update 13300.kt

--- Array/13300.kt
+++ Array/13300.kt
@@ -7,56 +7,21 @@ fun main() {
     val st = StringTokenizer(br.readLine())
     val n = st.nextToken().toInt()
     val k = st.nextToken().toInt()
-    val arr = IntArray(12)
+    val arr = Array(2) { IntArray(7) }
     for (i in 0 until n) {
         val st1 = StringTokenizer(br.readLine())
         val s = st1.nextToken().toInt()
         val y = st1.nextToken().toInt()
-        when (y) {
-            1 -> {
-                when (s) {
-                    0 -> arr[0]++
-                    1 -> arr[1]++
-                }
-            }
-            2 -> {
-                when (s) {
-                    0 -> arr[2]++
-                    1 -> arr[3]++
-                }
-            }
-            3 -> {
-                when (s) {
-                    0 -> arr[4]++
-                    1 -> arr[5]++
-                }
-            }
-            4 -> {
-                when (s) {
-                    0 -> arr[6]++
-                    1 -> arr[7]++
-                }
-            }
-            5 -> {
-                when (s) {
-                    0 -> arr[8]++
-                    1 -> arr[9]++
-                }
-            }
-            6 -> {
-                when (s) {
-                    0 -> arr[10]++
-                    1 -> arr[11]++
-                }
-            }
-        }
+        arr[s][y]++
     }
     var cnt = 0
-    for (i in 0 until 12) {
-        if (arr[i] % k == 0) {
-            cnt += arr[i] / k
-        } else {
-            cnt += arr[i] / k + 1
+    for (i in 0 until 2) {
+        for (j in 0..6) {
+            if (arr[i][j] % k == 0) {
+                cnt += arr[i][j] / k
+            } else {
+                cnt += arr[i][j] / k + 1
+            }
         }
     }
     println(cnt)
